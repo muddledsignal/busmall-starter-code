8dc4c21706dbc56bcb4552804e100328c73a5d19
muddledsignal <79wabvlw1dt7@opayq.com>
2018-05-17 16:35:03 -0700

    Delete item from cart working

--- 201d34-lab-14/js/cart.js
+++ 201d34-lab-14/js/cart.js
@@ -25,34 +25,45 @@ function clearCart() {
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
-  // TODO: Find the table body
   var cartTable = document.getElementById('cart');
 
-  // TODO: Iterate over the items in the cart
   for (var i in Cart) {
+    var idNum = 1 * i + 1;
     var cartTableTR = document.createElement('tr');
     var td1 = document.createElement('td');
     var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
 
     td1.textContent = Cart[i].name;
     td2.textContent = Cart[i].quantityChosen;
+    td3.textContent = 'Click to Remove X';
+
+    td3.id = idNum;
 
     cartTableTR.appendChild(td1);
     cartTableTR.appendChild(td2);
+    cartTableTR.appendChild(td3);
+
 
     cartTable.appendChild(cartTableTR);
   }
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
 
 function removeItemFromCart(event) {
 
   // TODO: When a delete link is clicked, rebuild the Cart array without that item
-  // TODO: Save the cart back to local storage
-  // TODO: Re-draw the cart table
+  var indexID = event.target.id;
+
+  if (indexID > 0) {
+    Cart.splice(1 * indexID -1, 1);
+
+    // TODO: Save the cart back to local storage
+    localStorage.setItem('cart', JSON.stringify(Cart));
+
+    // TODO: Re-draw the cart table
+    renderCart();
+  }
 
 }
 
